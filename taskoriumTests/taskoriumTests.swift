503856097f552c94f5efddd7cb22b2b1e170137b
Dan Douston <concordion@mac.com>
2026-02-03 13:58:26 -0500

    Initial Commit

---
+++ taskoriumTests/taskoriumTests.swift
@@ -0,0 +1,17 @@
+//
+//  taskoriumTests.swift
+//  taskoriumTests
+//
+//  Created by Dan Douston on 2/3/26.
+//
+
+import Testing
+@testable import taskorium
+
+struct taskoriumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
